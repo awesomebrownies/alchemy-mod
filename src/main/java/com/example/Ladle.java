3c33fcc881f6d78c352761cf5ce612bae5de2957
Karmaticow <114748592+Karmaticow@users.noreply.github.com>
2025-03-15 17:36:04 -0400

    if ladle is used on a cauldron, gives glass items

--- src/main/java/com/example/Ladle.java
+++ src/main/java/com/example/Ladle.java
@@ -1,7 +1,12 @@
 package com.example;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.item.Items;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
@@ -18,6 +23,29 @@ public class Ladle extends Item {
 
         user.playSound(SoundEvents.ENTITY_GENERIC_SPLASH, 1.0F, 1.0F);
         return ActionResult.SUCCESS;
+
+    }
+
+    @Override
+    public ActionResult useOnBlock(ItemUsageContext context) {
+
+        World world = context.getWorld();
+        BlockState blockstate = world.getBlockState(context.getBlockPos());
+        PlayerEntity player = context.getPlayer();
+
+        ItemStack bottle = null;
+        if (blockstate.getBlock() == Blocks.WATER_CAULDRON) {
+            bottle = new ItemStack(Items.GLASS_BOTTLE, 1);
+
+
+            if (player != null && bottle != null)
+                player.giveItemStack(bottle);
+
+            return ActionResult.SUCCESS;
+        }
+
+
+        return super.useOnBlock(context);
 
     }
 
